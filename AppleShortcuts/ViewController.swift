5a1a35d8f07350a23820d2be1783b7f476035d48
Hassan El Desouky <hassaneldesouky@icloud.com>
2019-08-08 21:19:35 +0200

    Initial Commit

---
+++ AppleShortcuts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleShortcuts
+//
+//  Created by Hassan El Desouky on 8/8/19.
+//  Copyright © 2019 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
